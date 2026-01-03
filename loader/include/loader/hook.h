02e0a3d91844c1f514a41edb2b8f4e829ad18815
Rai <56082069+raicool@users.noreply.github.com>
2026-01-02 20:52:21 -0500

    made it look fancier

--- loader/include/loader/hook.h
+++ loader/include/loader/hook.h
@@ -2,10 +2,36 @@
 
 #include "loader.h"
 
-// wrapper funcs for minhook
-int32_t LOADER_DLL loader_hook_create(void* target_ptr, void* detour_ptr, void** original_dptr);
-int32_t LOADER_DLL loader_hook_enable(void* target_ptr);
-int32_t LOADER_DLL loader_hook_remove(void* target_ptr);
-int32_t LOADER_DLL loader_hook_disable(void* target_ptr);
+// dllexport
+
+// 
+//
+//
+int32_t LOADER_DLL loader_hook_create(
+	IN void* target_ptr, 
+	IN void* detour_ptr, 
+	OUT void** original_dptr
+);
+
+//
+//
+//
+int32_t LOADER_DLL loader_hook_enable(
+	IN void* target_ptr
+);
+
+//
+//
+//
+int32_t LOADER_DLL loader_hook_remove(
+	IN void* target_ptr
+);
+
+//
+//
+//
+int32_t LOADER_DLL loader_hook_disable(
+	IN void* target_ptr
+);
 
 void log_minhook_status(int32_t status, uint32_t target = 0, uint32_t detour = 0);
