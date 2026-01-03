9431862e69052581251490e63982399c411c3438
Rai <56082069+raicool@users.noreply.github.com>
2026-01-02 20:49:49 -0500

    helper functions to get d3d11 variables

--- loader/include/loader/d3d11_hook.h
+++ loader/include/loader/d3d11_hook.h
@@ -5,12 +5,38 @@
 #include <d3d11.h>
 #include <imgui.h>
 
+LOADER_DLL ImGuiContext* loader_get_imgui_context();
+
+//
+// adds a function to be called when d3d frame is being presented
+//
+typedef void (*callback_func)(ID3D11RenderTargetView*, IDXGISwapChain*);
+LOADER_DLL void loader_add_present_callback(callback_func callback);
+
+//
+// @return pointer to rivals' process window
+//
+LOADER_DLL HWND loader_get_window();
+
+//
+// @return pointer to the current d3d11 device of rivals's main window
+//
+LOADER_DLL ID3D11Device* loader_get_d3d_device();
+
+//
+// @return pointer to the current d3d11 device context of rivals's main window
+//
+LOADER_DLL ID3D11DeviceContext* loader_get_d3d_device_context();
+
+//
+// @return pointer to the current d3d11 render target of rivals's main window
+//
+LOADER_DLL ID3D11RenderTargetView* loader_get_d3d_render_target();
+
+//
+// @return pointer to the current swapchain of rivals's main window
+//
+LOADER_DLL IDXGISwapChain* loader_get_dx_swapchain();
+
 void hook_d3d11();
 void unhook_d3d11();
-
-static ImGuiContext* imgui_context = nullptr;
-LOADER_DLL ImGuiContext* loader_get_imgui_context();
-LOADER_DLL ID3D11Device* get_d3d_device();
-LOADER_DLL ID3D11DeviceContext* get_d3d_device_context();
-LOADER_DLL ID3D11RenderTargetView* get_d3d_render_target();
-LOADER_DLL IDXGISwapChain* get_dx_swapchain();
