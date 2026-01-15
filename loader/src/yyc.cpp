cc87785b394e09521bba776789b592b4c95f80da
Rai <56082069+raicool@users.noreply.github.com>
2026-01-15 17:54:55 -0500

    add __impl prefix to UGMMS functions

--- loader/src/yyc.cpp
+++ loader/src/yyc.cpp
@@ -9,21 +9,21 @@ LOADER_DLL CRoom* loader_get_room_by_index(uint32_t room_idx)
 
 LOADER_DLL int loader_yyc_get_funcid(const char* name)
 {
-	return get_func_info(name).id;
+	return __impl_get_func_info(name).id;
 }
 
 LOADER_DLL void* loader_get_yyc_func_ptr(const char* name)
 {
-	return get_func_info(name).ptr;
+	return __impl_get_func_info(name).ptr;
 }
 
 LOADER_DLL GMLVar* loader_yyc_call_func(const char* name, size_t args_count, GMLVar** args)
 {
-	uint32_t id = get_func_info(name).id;
+	uint32_t id = __impl_get_func_info(name).id;
 	return loader_yyc_call_func(id, args_count, args);
 }
 
 LOADER_DLL GMLVar* loader_yyc_call_func(uint32_t id, size_t args_count, GMLVar** args)
 {
-	return gml_call_func(id, args_count, args);
+	return __impl_gml_call_func(id, args_count, args);
 }
